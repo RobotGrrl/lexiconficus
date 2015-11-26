cdaa5a3767a0655e42f4be9687f98ebbe9dea2ba
RobotGrrl <erin@robotgrrl.com>
2015-11-26 10:28:49 -0500

    this configures passport

--- VotetronTutorial/index.js
+++ VotetronTutorial/index.js
@@ -4,6 +4,25 @@ var express = require('express'),
 
 var app = express();
 
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GitHubStrategy(
+    {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: process.env.GITHUB_CALLBACK
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // on successful auth
+    }
+));
+
 // configure Express
 app.use(express.logger());
 app.use(express.cookieParser());
@@ -16,6 +35,13 @@ app.use(app.router);
 
 app.get('/', function(req, res){
     res.send('hello world');
+});
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }), function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
 });
 
 app.listen(process.env.PORT || 5000); 
